e754a6d2611c63a889ced515f14fd086a438e701
Saroj Meshram <53329367+meshramsaroj@users.noreply.github.com>
2021-09-15 16:55:22 +0530

    chore: code of lead.js file is moved from bloomstack_core to erpnext (#1657)
    Co-authored-by: meshramsaroj <saroj@digithinit.com>

--- erpnext/crm/doctype/lead/lead.js
+++ erpnext/crm/doctype/lead/lead.js
@@ -98,3 +98,34 @@ erpnext.LeadController = frappe.ui.form.Controller.extend({
 });
 
 $.extend(cur_frm.cscript, new erpnext.LeadController({ frm: cur_frm }));
+
+frappe.ui.form.on("Lead", {
+	setup: (frm) => {
+		frm.set_query("region", { "is_group": 1 });
+	},
+
+	onload: (frm) => {
+		frm.set_query("territory", () => {
+			if (frm.doc.region) {
+				return {
+					query: "erpnext.crm.doctype.lead.filter_territory",
+					filters: {
+						region: frm.doc.region
+					}
+				};
+			}
+		});
+
+		if (!frm.doc.account_opened_date) {
+			frappe.db.get_value("Customer", { "lead_name": frm.doc.name }, ["opening_date", "creation"], (r) => {
+				if (r) {
+					if (r.opening_date) {
+						frm.set_value("account_opened_date", r.opening_date);
+					} else {
+						frm.set_value("account_opened_date", r.creation);
+					}
+				}
+			});
+		}
+	}
+});
