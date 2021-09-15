59383b6894d1ea063ca692ee23927c27b9f74a82
Myuddin Khatri <53251406+MyuddinKhatri@users.noreply.github.com>
2021-09-15 11:18:54 +0530

    fix: move driver.py code to erpnext from bloomstack_core (#1650)

--- erpnext/hr/doctype/driver/driver.py
+++ erpnext/hr/doctype/driver/driver.py
@@ -3,7 +3,15 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
 
 class Driver(Document):
-	pass
+	def validate(self):
+		self.get_employee_from_user()
+
+	def get_employee_from_user(self):
+		if self.user_id:
+			employee = frappe.db.get_value("Employee", {"user_id": self.user_id})
+			if employee:
+				self.employee = employee
