74b756b187cbab075df59d34f655d0276fd0f9db
Saroj Meshram <53329367+meshramsaroj@users.noreply.github.com>
2021-09-16 13:16:24 +0530

    chore: move stock_entry.js file to erpnext from bloomstack_core (#1646)
    Co-authored-by: meshramsaroj <saroj@digithinit.com>

--- erpnext/stock/doctype/stock_entry/stock_entry.js
+++ erpnext/stock/doctype/stock_entry/stock_entry.js
@@ -538,10 +538,39 @@ frappe.ui.form.on('Stock Entry Detail', {
 		frm.events.set_serial_no(frm, cdt, cdn, () => {
 			frm.events.get_warehouse_details(frm, cdt, cdn);
 		});
+
+		const row = locals[cdt][cdn];
+		if (row.s_warehouse) {
+			frappe.db.get_value("Warehouse", { "name": row.s_warehouse }, ["customer", "supplier"], (r) => {
+				frappe.model.set_value(cdt, cdn, "customer", r.customer || row.customer);
+				frappe.model.set_value(cdt, cdn, "supplier", r.supplier || row.supplier);
+				refresh_field("items");
+			})
+		} else if (row.t_warehouse) {
+			frm.script_manager.trigger('t_warehouse', cdt, cdn);
+		} else {
+			frappe.model.set_value(cdt, cdn, "customer", "");
+			frappe.model.set_value(cdt, cdn, "supplier", "");
+		}
 	},
 
 	t_warehouse: function(frm, cdt, cdn) {
 		frm.events.get_warehouse_details(frm, cdt, cdn);
+
+		const row = locals[cdt][cdn];
+
+		if (row.t_warehouse) {
+			frappe.db.get_value("Warehouse", { "name": row.t_warehouse }, ["customer", "supplier"], (r) => {
+				frappe.model.set_value(cdt, cdn, "customer", r.customer || row.customer);
+				frappe.model.set_value(cdt, cdn, "supplier", r.supplier || row.supplier);
+				refresh_field("items");
+			})
+		} else if (row.s_warehouse) {
+			frm.script_manager.trigger("s_warehouse", cdt, cdn);
+		} else {
+			frappe.model.set_value(cdt, cdn, "customer", "");
+			frappe.model.set_value(cdt, cdn, "supplier", "");
+		}
 	},
 
 	basic_rate: function(frm, cdt, cdn) {
